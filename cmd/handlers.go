aca183cd14f6b7c349c353cdac3459719513e5f0
brayandev <anttonybrayann@gmail.com>
2019-04-26 14:18:33 -0300

    implements handler to create a course

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/Course/course"
+	"go.uber.org/zap"
+)
+
+func createCourse(service course.Service, logger *zap.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {}
+}
